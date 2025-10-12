3fa906fe58f0914adfac97f0068512a74156c7ef
Paulo <paulorobertofer98@gmail.com>
2025-10-12 11:04:32 -0300

    fix db

--- src/main/java/com/paledev/hobbsched/repositories/ServiceRepository.java
+++ src/main/java/com/paledev/hobbsched/repositories/ServiceRepository.java
@@ -1,10 +1,11 @@
 package com.paledev.hobbsched.repositories;
 
+import com.paledev.hobbsched.models.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
+
 
 @Repository
-public interface ServiceRepository extends JpaRepository<Service,Long> {
+public interface ServiceRepository extends JpaRepository<Service,Long > {
 
 }
